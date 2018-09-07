5a3bf2524d77bac68c27ee83fed7bbdf8defb6cb
mbaljeetsingh <bsinghs.007@gmail.com>
2018-09-07 12:53:32 +0530

    adding headlines horizontal slides with chip

--- src/app/pages/headlines/headlines.page.ts
+++ src/app/pages/headlines/headlines.page.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-headlines',
   templateUrl: './headlines.page.html',
-  styleUrls: ['./headlines.page.scss'],
+  styleUrls: ['./headlines.page.scss']
 })
 export class HeadlinesPage implements OnInit {
+  categories = [
+    'India',
+    'World',
+    'Business',
+    'Technology',
+    'Entertainment',
+    'Sports',
+    'Science'
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
