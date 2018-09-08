5bc4be99df29d68f63718dc360eeef2d31710ac6
mbaljeetsingh <bsinghs.007@gmail.com>
2018-09-08 14:49:14 +0530

    Show Source Articles

--- src/app/pages/source/source.page.ts
+++ src/app/pages/source/source.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-source',
   templateUrl: './source.page.html',
-  styleUrls: ['./source.page.scss'],
+  styleUrls: ['./source.page.scss']
 })
 export class SourcePage implements OnInit {
-
-  constructor() { }
+  news;
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
+    this.news = this.newsService.getData('everything?sources=abc-news');
   }
-
 }
