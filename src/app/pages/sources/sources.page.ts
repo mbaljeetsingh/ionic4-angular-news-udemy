69d9a932da72fa3975283b03a05b14ed1abc4d86
mbaljeetsingh <bsinghs.007@gmail.com>
2018-09-08 16:46:00 +0530

    Show Toast Message

--- src/app/pages/sources/sources.page.ts
+++ src/app/pages/sources/sources.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../../services/news.service';
 import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sources',
@@ -11,13 +12,25 @@ export class SourcesPage implements OnInit {
   sources;
   fakeSources = new Array(20);
   term = '';
-  constructor(private newsService: NewsService, private storage: Storage) {}
+  constructor(
+    private newsService: NewsService,
+    private storage: Storage,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     // this.sources = this.newsService.getData('sources');
     this.newsService.getData('sources').subscribe(sources => {
       this.sources = sources['sources'];
     });
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Added To Favorite.',
+      duration: 2000
+    });
+    toast.present();
   }
 
   favorite(source) {
@@ -29,6 +42,7 @@ export class SourcesPage implements OnInit {
       }
       items.push(source);
       this.storage.set('favorite', JSON.stringify(items));
+      this.presentToast();
     });
   }
 }
