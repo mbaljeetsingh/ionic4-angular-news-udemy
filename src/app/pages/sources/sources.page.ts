5b80805f0cba39a0befee022d19b32cb8e5948cb
mbaljeetsingh <bsinghs.007@gmail.com>
2018-09-08 12:08:03 +0530

    Sources page ui and get data

--- src/app/pages/sources/sources.page.ts
+++ src/app/pages/sources/sources.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-sources',
   templateUrl: './sources.page.html',
-  styleUrls: ['./sources.page.scss'],
+  styleUrls: ['./sources.page.scss']
 })
 export class SourcesPage implements OnInit {
-
-  constructor() { }
+  sources;
+  fakeSources = new Array(20);
+  constructor(private newsService: NewsService) {}
 
   ngOnInit() {
+    this.sources = this.newsService.getData('sources');
   }
-
 }
