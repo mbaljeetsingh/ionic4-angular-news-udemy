99a404542da71e1bf77a6bb155fbded079038ab5
mbaljeetsingh <bsinghs.007@gmail.com>
2018-09-08 16:33:01 +0530

    Refresh button favorites

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -12,11 +12,19 @@ export class FavoritesPage implements OnInit {
   constructor(private storage: Storage) {}
 
   ngOnInit() {
+    this.getFavorites();
+  }
+
+  getFavorites() {
     this.storage.get('favorite').then(val => {
       if (val != null) {
         this.sources = JSON.parse(val);
       }
       console.log(this.sources);
     });
+  }
+
+  onRefresh() {
+    this.getFavorites();
   }
 }
